fbf60ab7e25b3a05d02a4b439eff5bed796b9890
Nima karimi <nima.karimi@live.com>
2018-02-13 12:11:35 +0330

    calender app install

--- polls/models.py
+++ polls/models.py
@@ -6,10 +6,15 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("Date published")
+    pub_date = models.DateTimeField("تاریخ انتشار")
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
